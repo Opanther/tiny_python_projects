eb7366bb59c0cb00f6cde0a790f453680f8979cb
opanther <tom.oredein@gmail.com>
2022-11-15 10:53:37 +0000

    working with strings

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Author : tomioredein <tomioredein@localhost>
+Date   : 2022-11-11
+Purpose: Crow's Nest
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Working with strings",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("word", metavar="word", help="A word")
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main() -> object:
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word = args.word
+    article = "an" if word[0].lower() in "aeiou" else "a"
+
+    print(f"Ahoy, Captain, {article} {word} off the larboard bow!")
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
